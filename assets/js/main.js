322ce9d6856bbf98bb1c6099752ea4de5676b4f3
Leonardo <leonardo.bongianni@gmail.com>
2021-04-13 00:16:03 +0200

    Milestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,21 +4,34 @@ var app = new Vue (
   {
     el: "#root",
     data: {
-      searchMovie: "",
+      search: "",
       movies: [],
-      serie: [],
+      series: [],
     },
     methods: {
       getMovie: function (){
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=e23a6721c7b2483cf2f94f3a47f7288d&language=it&query=${this.searchMovie}`)
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=e23a6721c7b2483cf2f94f3a47f7288d&language=it&query=${this.search}`)
           .then((response) => {
           console.log(response.data.results);
           this.movies = response.data.results;
           console.log(this.movies);
 
           this.movies.sort((a, b) => (b.vote_average - a.vote_average));
+
+        axios.get(`https://api.themoviedb.org/3/search/tv?api_key=e23a6721c7b2483cf2f94f3a47f7288d&language=it&query=${this.search}`)
+          .then((response)=> {
+            // console.log(response);
+            this.series = response.data.results;
+            // console.log(this.series);
+            this.series.forEach((episodi) => {
+              // console.log(episodi);
+              this.movies.push(episodi);
+            });
+
+          })
         })
-      }
+      },
+
     }
   }
 );
