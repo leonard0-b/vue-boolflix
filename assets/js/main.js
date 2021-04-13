1899c0d53e71b98341033f0e44bcfebd8fa4b81a
Leonardo <leonardo.bongianni@gmail.com>
2021-04-13 09:53:31 +0200

    Ottimizzato link di chiamata all'api così da renderlo modificabile in futuro

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,15 @@ var app = new Vue (
     el: "#root",
     data: {
       search: "",
+      api_key: "e23a6721c7b2483cf2f94f3a47f7288d",
+      lang: "it",
+      uri: "https://api.themoviedb.org/3",
       movies: [],
       series: [],
     },
     methods: {
       getMovie: function (){
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=e23a6721c7b2483cf2f94f3a47f7288d&language=it&query=${this.search}`)
+        axios.get(`${this.uri}/search/movie?api_key=${this.api_key}&language=${this.lang}&query=${this.search}`)
           .then((response) => {
           console.log(response.data.results);
           this.movies = response.data.results;
@@ -18,7 +21,7 @@ var app = new Vue (
 
           this.movies.sort((a, b) => (b.vote_average - a.vote_average));
 
-        axios.get(`https://api.themoviedb.org/3/search/tv?api_key=e23a6721c7b2483cf2f94f3a47f7288d&language=it&query=${this.search}`)
+        axios.get(`${this.uri}/search/tv?api_key=${this.api_key}&language=${this.lang}&query=${this.search}`)
           .then((response)=> {
             // console.log(response);
             this.series = response.data.results;
